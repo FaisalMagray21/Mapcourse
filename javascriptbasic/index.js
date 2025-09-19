0c2b8b47d296501fb2001768ed77e7a3837f5228
FaisalMagray21 <136814501+FaisalMagray21@users.noreply.github.com>
2025-09-19 02:40:37 -0700

    lab task related to function

--- javascriptbasic/index.js
+++ javascriptbasic/index.js
@@ -41,20 +41,101 @@
 // //console.log(outer()());
 // //conceptofclosure
 
-function outer() {
-  let count = 0;
+// function outer() {
+//   let count = 0;
 
-  function inner() {
-    count++;
-    return count;
+//   function inner() {
+//     count++;
+//     return count;
+//   }
+
+//   return inner;
+// }
+
+// const counter = outer(); 
+
+// console.log(counter()); // 1
+// console.log(counter()); // 2
+// console.log(counter()); // 3
+
+//calculator
+class Calculator {
+  add = (a, b) => {
+    return a + b;
+  };
+  subtract=(a,b)=>{
+    return a-b;
   }
-
-  return inner;
+  multiply=(a,b)=>{
+    return a*b;
+  }
+  divide=(a,b)=>{
+    if(b===0){
+      return "Error: Division by zero";
+    }
+    return a / b;
+  }
 }
+const calc = new Calculator();
+console.log(calc.add(5, 3));
+console.log(calc.subtract(5, 3));
+console.log(calc.multiply(5, 3));
+console.log(calc.divide(5, 0));
+console.log(calc.divide(5, 2));
 
-const counter = outer(); 
+//findlargestnumber
 
-console.log(counter()); // 1
-console.log(counter()); // 2
-console.log(counter()); // 3
+let largest=()=>{
+  let arr=[3, 5, 7, 2, 8];  
+
+    let max=arr[0];
+    if(arr.length===0){
+        return null;
+    }
+  for(let i=0;i<arr.length;i++){
+    if(max<arr[i]){
+        max=arr[i];
+    }
+  }
+   console.log("Max value"+max);
+
+}
+largest();
+
+let table=(num)=>{
+  for(let i=1;i<=10;i++){
+    console.log(`${num} x ${i} = ${num*i}`);
+  }
+}
+table(4);
+let evenodd=(num)=>{
+  if(num%2===0){
+    console.log("Even number");
+  }
+  else{
+    console.log("Odd number");
+  }
+}
+evenodd(5);
+evenodd(4);
+let factorial=(num)=>{
+  let fact=1;
+  for(let i=1;i<=num;i++){
+    fact*=i;
+  }
+  return fact;
+}
+console.log(factorial(5));
+
+
+let function1=(val)=>{
+  console.log(val);
+}
+let function2=(callback)=>{
+  let num=10;
+  callback(num);
+}
+function2(function1);
+
+
 
